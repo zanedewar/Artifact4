b4104d3137eff240d0b49ebdb93f2d2f8949caee
zanedewar <zane_dewar@hotmail.com>
2021-05-22 19:26:34 -0700

    Update Artifact4js.js

--- Artifact4js.js
+++ Artifact4js.js
@@ -13,9 +13,20 @@ function validateForm() {
 	var errorMessages = "";
 	
 	var firstName = document.getElementById("FirstName");
-	if(firstName === "null" || firstName ==="" || firstName.length > 20) 
+	if(firstName === "null" || firstName ==="" || firstName.length > 20 || !checkCharacters(firstName)) 
 		errorMessages += "<p>The first name is required and cannot be greater than 20 characters</p>";
-	
+	else 
+		validFirst = true;
 	
 	document.getElementById("errorMessages") = errorMessages;
+	
+}
+function checkCharacters(word) {
+	for(var i = 0; i < word.length; i++) {
+		var currChar = word.charCodeAt(i);
+		if(!(currChar > 64 && currChar < 91) && !(currChar > 96 && currChar < 123))
+			return false;
+	}
+	return true;
+	
 }
